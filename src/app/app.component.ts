d7242801d99c71aaec697c9bb76a0485aab6cced
Aman Kumar <aksingh230502@gmail.com>
2024-11-29 12:03:57 +0530

    forgot password page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { SignupPageComponent } from "./pages/signup-page/signup-page.component";
+import { ForgotPasswordComponent } from "./pages/forgot-password/forgot-password.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent, LoginPageComponent, SignupPageComponent],
+  imports: [RouterOutlet, HomePageComponent, LoginPageComponent, SignupPageComponent, ForgotPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     // { path: 'login', component: LoginPageComponent },
     // { path: 'signup', loadComponent: () => import('./pages/signup/signup.component') },
 //     { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login by default
-//   { path: 'login', component: LoginPageComponent },
+  { path: 'login', component: LoginPageComponent },
 //   { path: 'home', component: HomePageComponent },
-  { path: 'signup', component: SignupPageComponent },
+//   { path: 'signup', component: SignupPageComponent },
+{ path: 'forgotPassword', component: ForgotPasswordComponent },
 
 ];
